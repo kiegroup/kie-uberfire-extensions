aecfa178f30bde1c27c5de5df9f72b8bf58328ae
Michael Biarnes Kiefer <mbiarnes@redhat.com>
2015-12-16 12:20:25 +0100

    changes in license header

--- kie-uberfire-social-activities/kie-uberfire-social-activities-backend/src/test/java/org/kie/uberfire/social/activities/server/SocialUserPersistenceProducerTest.java
+++ kie-uberfire-social-activities/kie-uberfire-social-activities-backend/src/test/java/org/kie/uberfire/social/activities/server/SocialUserPersistenceProducerTest.java
@@ -8,7 +8,7 @@ import org.uberfire.java.nio.file.FileSystem;
 
 import javax.inject.Named;
 /*
- * Copyright 2015 JBoss Inc
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
