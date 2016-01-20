17107fac1b2f2ce5b1cd2ff7592f80c3e732d801
Max Barkley <mbarkley@redhat.com>
2016-01-20 17:51:14 -0500

    Update to Errai 4.0, GWT 2.8.0-beta1, and Java 8.

--- kie-uberfire-social-activities/kie-uberfire-social-activities-client/src/main/java/org/kie/uberfire/social/activities/client/gravatar/GravatarUrlBuilder.java
+++ kie-uberfire-social-activities/kie-uberfire-social-activities-client/src/main/java/org/kie/uberfire/social/activities/client/gravatar/GravatarUrlBuilder.java
@@ -15,9 +15,9 @@
 
 package org.kie.uberfire.social.activities.client.gravatar;
 
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-import com.google.gwt.core.client.impl.Md5Digest;
 import com.googlecode.gwt.crypto.bouncycastle.util.encoders.Hex;
 /**
  * Url Builder
@@ -54,7 +54,7 @@ public class GravatarUrlBuilder {
     private String hash( String email ) {
         try {
             String cleanEmail = email.trim().toLowerCase();
-            return new String( Hex.encode( Md5Digest.getInstance( "MD5" ).digest( cleanEmail.getBytes() ) ) );
+            return new String( Hex.encode( MessageDigest.getInstance( "MD5" ).digest( cleanEmail.getBytes() ) ) );
         } catch ( NoSuchAlgorithmException e ) {
             throw new RuntimeException( "MD5 implementation not found", e );
         }
